892387798bada7f551724212822e9097690c097c
Ramesh <ramesh1982s@gmail.com>
2018-01-16 00:09:47 -0500

    Initial Commit

--- ruby_examples/string_examples.rb
+++ ruby_examples/string_examples.rb
@@ -1,4 +1,4 @@
-module Palindrome
+class Palindrome
   def self.check_palindrome(word)
     if word == word.reverse
       puts "Polindrome"
@@ -9,4 +9,26 @@ module Palindrome
 end
 
 Palindrome.check_palindrome('rameshhsemar')
+word = 1
+puts 'abc'.__id__
+puts 'abc'.__id__
+puts :abc.__id__
+puts :abc.__id__
+puts Palindrome.__id__
+puts word.object_id
+puts word.__id__
+
+a = Palindrome.new
+puts a.__id__
+a = Palindrome.new
+puts a.__id__
+# puts obj.__id__
+b = Object.new
+puts b.__id__
+puts self
+
+puts Object.new.object_id
+puts Object.object_id
+puts Time.now
+
 
